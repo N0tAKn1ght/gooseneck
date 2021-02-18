c47ee1f6bd760ab82eb4e2ecae251e4441736c16
N0tAKn1ght <77507902+N0tAKn1ght@users.noreply.github.com>
2021-02-18 13:06:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import discord
+from discord.ext import commands
 import os
 
-client = discord.Client()
+client = commands.Bot(command_prefix=".")
+token = os.getenv("DISCORD_BOT_TOKEN")
 
 @client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+async def on_ready() :
+    await client.change_presence(status = discord.Status.idle, activity = discord.Game("Listening to .help"))
+    print("I am online")
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@client.command()
+async def ping(ctx) :
+    await ctx.send(f"🏓 Pong with {str(round(client.latency, 2))}")
 
-    if message.content.startswith('.hello'):
-        await message.channel.send('Hello!')
+@client.command(name="whoami")
+async def whoami(ctx) :
+    await ctx.send(f"You are {ctx.message.author.name}")
 
-client.run('ODA4NDE1ODg5ODc1NjY0OTI3.YCGN9w.qIBD2q-uteQ_YIyEYeubBFVCNhk')
+@client.command()
+async def clear(ctx, amount=3) :
+    await ctx.channel.purge(limit=amount)
+
+
+client.run(token)
