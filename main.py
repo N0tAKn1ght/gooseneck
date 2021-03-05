bfa06a224199caf798b45876f5c97fac089ac29f
N0tAKn1ght <77507902+N0tAKn1ght@users.noreply.github.com>
2021-03-04 21:26:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import discord
 import asyncio
 import os
 from discord.ext import commands
+from discord.ext import files
+
+from PIL import Image, ImageDraw, ImageFont
+import io
 
 client = commands.Bot(command_prefix = ".")
 token = 'ODA4NDE1ODg5ODc1NjY0OTI3.YCGN9w.qIBD2q-uteQ_YIyEYeubBFVCNhk'
