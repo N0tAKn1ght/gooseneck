380e15ed45e39fec74ec21e53e7de561ab3e79ac
N0tAKn1ght <77507902+N0tAKn1ght@users.noreply.github.com>
2021-03-04 20:50:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,58 +9,73 @@ token = 'ODA4NDE1ODg5ODc1NjY0OTI3.YCGN9w.qIBD2q-uteQ_YIyEYeubBFVCNhk'
 
 @client.event
 async def on_ready() :
-    await client.change_presence(status = discord.Status.idle, activity = discord.Game("Listening to .hep"))
+    await client.change_presence(status = discord.Status.idle, activity = discord.Game("Listening to .help"))
     print("I am online")
 
-@client.command()
+@client.command(pass_context = True)
 async def ping(ctx) :
-    await ctx.send(f":ping_pong: Pong with {str(round(client.latency, 2))}")
+    embed = discord.Embed(title = "card check", description = (f":ping_pong: Pong with {str(round(client.latency, 2))}")), color = 0xa1ffb0)
+    await ctx.send(embed = embed)
 
-@client.command(name="whoami")
-async def whoami(ctx) :
-    await ctx.send(f"You are {ctx.message.author.name}")
+@client.command(pass_context = True)
+async def whoami(ctx):
+    embed = discord.Embed(title = "card check", description = (f"You are {ctx.message.author.name}")), color = 0xa1ffb0)
+    await ctx.send(embed = embed)
 
 # Says hello, used to see if bot is online
-@client.command(name="hello")
-async def hello(ctx) :
-    await ctx.send("Howdy!")
+@client.command(pass_context = True)
+async def hello(ctx):
+    embed = discord.Embed(title = "card check", description = ("Howdy!")), color = 0xa1ffb0)
+    await ctx.send(embed = embed)
 
 # Help Command, will redirect user to site to see list of command
 # Quick and easy guide to start 
-@client.command(name="hep")
-async def hep(ctx) :
-    await ctx.send("This is the help command")
+@client.command(pass_context = True)
+async def hep(ctx):
+    embed = discord.Embed(title = "card check", description = ("This is the help command")), color = 0xa1ffb0)
+    await ctx.send(embed = embed)
 
 # In-Depth help command, used for full list of commands
-@client.command(name="hep_cmd")
-async def hep_cmd(ctx) :
-    await ctx.send("In-Depth help")
+@client.command(pass_context = True)
+async def hep_cmd(ctx):
+    embed = discord.Embed(title = "card check", description = ("In-Depth help")), color = 0xa1ffb0)
+    await ctx.send(embed = embed)
 
 # User can get to our site, for more info, invite to new server, etc
-@client.command(name="home")
-async def home(ctx) :
-    await ctx.send("Will send user to home page site :D")
+@client.command(pass_context = True)
+async def home(ctx):
+    embed = discord.Embed(title = "card check", description = ("Will send user to home page site :D")), color = 0xa1ffb0)
+    await ctx.send(embed = embed)
 
 # Will initiate user to base, give a preset amount of resources to get started with the game
-@client.command(name="start")
-async def start(ctx) :
-    await ctx.send("Initiate game")
+@client.command(pass_context = True)
+async def start(ctx):
+    embed = discord.Embed(title = "card check", description = ("Initiate game")), color = 0xa1ffb0)
+    await ctx.send(embed = embed)
 
 # Will perform functions (To be imported later), to give user a card and decide rarity
-@client.command(name="roll")
-async def roll(ctx) :
-    await ctx.send("Will run 2 random functions, deciding card and rarity they obtain")
+@client.command(pass_context = True)
+async def roll(ctx):
+    embed = discord.Embed(title = "card check", description = ("Will run 2 random functions, deciding card and rarity they obtain")), color = 0xa1ffb0)
+    await ctx.send(embed = embed)
 
 # Given a card ID, will display information about card
 # i.e. Name of Card, date obtained, rarity, etc
-@client.command(name="info")
-async def hep_cmd(ctx) :
+@client.command(pass_context = True)
+async def info(ctx):
     embed = discord.Embed(title = "card check", description = ("Display info about given card")), color = 0xa1ffb0)
     await ctx.send(embed = embed)
-    
+
 @client.command(pass_context = True)
 async def randomNumber(ctx):
     embed = discord.Embed(title = "card check", description = (random.randint(1, 18)), color = 0xa1ffb0)
+    await ctx.send(embed = embed)
+
+
+
+@client.command(pass_context = True)
+async def image(ctx):
+    embed = discord.Embed(title = "alpaca 10", file = discord.File('13868967033_82c85fbc18_c(1).jpg'), color = 0xa1ffb0)
     await ctx.send(embed = embed)
 
 client.run(token)
